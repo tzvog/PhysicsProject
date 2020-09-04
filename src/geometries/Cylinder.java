c8d3759251c9915980f3204039b5da959a7afc60
tzvi <timothy.vogel@mail.huji.ac.il>
2020-09-04 12:01:06 +0300

    added Cylinder

---
+++ src/geometries/Cylinder.java
@@ -0,0 +1,21 @@
+package geometries;
+
+/**
+ * a class to implement a cylinder
+ */
+public class Cylinder extends Tube {
+
+    /* local variable declaration */
+    double _height;
+
+    /* Getters */
+
+    /**
+     * a function to get the height
+     * @return the height
+     */
+    public double get_height() {
+        return _height;
+    }
+
+}
