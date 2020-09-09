1c9fee1be3d421da36cdc1f0bfad2de8427126c6
tzvi <timothy.vogel@mail.huji.ac.il>
2020-09-09 10:39:39 +0300

    finished vector tests

--- src/unittests/VectorTest.java
+++ src/unittests/VectorTest.java
@@ -11,7 +11,8 @@ import static org.junit.Assert.*;
 
 public class VectorTest {
 
-    Vector v;
+    Vector v, v2, v3, v4, v5;
+
 
     @Before
     public void setUp() throws Exception {
@@ -105,14 +106,14 @@ public class VectorTest {
         v = new Vector(2, 3, 5);
 
         try {
-            Vector v2 = new Vector(-2, -3, -5);
-            Vector v3 = v.add(v2);
+            v2 = new Vector(-2, -3, -5);
+            v3 = v.add(v2);
             fail("bad zero vector");
         }
         catch (IllegalArgumentException ex){}
 
-        Vector v4 = new Vector(1, 2, 3);
-        Vector v5 = new Vector(v.add(v4));
+        v4 = new Vector(1, 2, 3);
+        v5 = new Vector(v.add(v4));
 
         assertEquals(3 , v5.get_point().get_firstCoordinate().get(), 0.0);
         assertEquals(5 , v5.get_point().get_secondCoordinate().get(), 0.0);
@@ -126,14 +127,14 @@ public class VectorTest {
         v = new Vector(2, 3, 5);
 
         try {
-            Vector v2 = new Vector(2, 3, 5);
-            Vector v3 = v.subtract(v2);
+            v2 = new Vector(2, 3, 5);
+            v3 = v.subtract(v2);
             fail("bad zero vector");
         }
         catch (IllegalArgumentException ex){}
 
-        Vector v4 = new Vector(1, 2, 3);
-        Vector v5 = new Vector(v.subtract(v4));
+        v4 = new Vector(1, 2, 3);
+        v5 = new Vector(v.subtract(v4));
 
         assertEquals(1 , v5.get_point().get_firstCoordinate().get(), 0.0);
         assertEquals(1 , v5.get_point().get_secondCoordinate().get(), 0.0);
@@ -165,33 +166,77 @@ public class VectorTest {
 
     @Test
     public void dotProduct() {
+
+        v = new Vector(2, 3, 5);
+        v2 = new Vector(2, 3, 5);
+        assertEquals(38 , v.dotProduct(v2), 0.0);
+
+        v = new Vector(1, 0, 0);
+        v2 = new Vector(0,1,1);
+        assertEquals(0 , v.dotProduct(v2), 0.0);
+
     }
 
     @Test
     public void crossProduct() {
+
+        v = new Vector(1, 0, 0);
+        v2 = new Vector(0, 1, 0);
+        v3 = v.crossProduct(v2);
+
+        assertEquals("basic cross product",
+                new Point3D(0,0,1), v3.get_point());
     }
 
     @Test
     public void lengthSquared() {
+
+        v = new Vector(1, 0, 0);
+        assertEquals(1, v.lengthSquared(), 0);
+
+        v2 = new Vector(2,3,5);
+        assertEquals(38, v2.lengthSquared(), 0);
+
+        v3 = new Vector(-2,-3,-5);
+        assertEquals(38, v3.lengthSquared(), 0);
     }
 
     @Test
     public void length() {
+
+        v = new Vector(1, 0, 0);
+        assertEquals(1, v.length(), 0);
+
+        v2 = new Vector(-2,-3,-5);
+        assertEquals(Math.sqrt(38), v2.length(), 0);
+
+        v3 = new Vector(2,3,5);
+        assertEquals(Math.sqrt(38), v3.length(), 0);
     }
 
     @Test
     public void normalize() {
+
+        v = new Vector(1, 0, 0);
+        assertEquals(1, v.normalize().length(), 0);
+
+        v2 = new Vector(-2,-3,-5);
+        assertEquals(1, v2.normalize().length(), 0);
+
+        v3 = new Vector(2,3,5);
+        assertEquals(1, v3.normalize().length(), 0);
     }
 
     @Test
     public void normalized() {
-    }
 
-    @Test
-    public void equals1() {
-    }
+        v = new Vector(1, 0, 0);
+        assertEquals(1, v.normalized().length(), 0);
 
-    @Test
-    public void toString1() {
+        v2 = new Vector(-2,-3,-5);
+        assertEquals(1, v2.normalized().length(), 0);
+
+        v3 = new Vector(2,3,5);
+        assertEquals(1, v3.normalized().length(), 0);
     }
 }
