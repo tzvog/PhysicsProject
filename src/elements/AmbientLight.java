34862ca108486a64de0e687504d6dc923d298fa7
tzvi <timothy.vogel@mail.huji.ac.il>
2020-09-24 14:05:41 +0300

    ambient light class

---
+++ src/elements/AmbientLight.java
@@ -0,0 +1,102 @@
+package elements;
+
+import primitives.Point3D;
+
+import java.awt.Color;
+
+/**
+ * a class to represent a Ambient Light
+ */
+public class AmbientLight {
+
+
+    /* local variable declaration */
+    private Color _color;
+    private Double _ka;
+
+    /* Getters */
+
+    /**
+     * gets  the color
+     * @return the color
+     */
+    public Color get_color() {
+        return _color;
+    }
+
+    /**
+     * gets the ka
+     * @return the ka
+     */
+    public Double get_ka() {
+        return _ka;
+    }
+
+    /* Constructors */
+
+    /**
+     * a constructor that receives two values
+     * @param c the color we receive
+     * @param ka gets the intensity
+     */
+    public AmbientLight(Color c, double ka)
+    {
+        this._color = c;
+        this._ka = ka;
+    }
+
+    /**
+     * a constructor that gets the light
+     * @param a red intensity
+     * @param b green intensity
+     * @param c blue intensity
+     */
+    public AmbientLight(int a, int b, int c){
+        this(new Color(a, b, c), 1.0);
+    }
+
+    /**
+     * default constructor
+     */
+    public AmbientLight(){
+        this(255, 255, 255);
+    }
+
+    /* Functions */
+
+    /**
+     * gets the intensity of the light times the color
+     * @param point the point we need
+     * @return the color
+     */
+    public Color getIntesity(Point3D point){
+
+        return new Color((int)(this._color.getRed() * this._ka),
+                (int)(this._color.getGreen() * this._ka),
+                (int)(this._color.getBlue() * this._ka));
+    }
+
+
+    /**
+     * checks if two points have the same values
+     * @param obj the other point
+     * @return if they are equal
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof AmbientLight)) return false;
+        AmbientLight oth = (AmbientLight) obj;
+        return this._color.equals(oth._color) && this._ka.equals(oth._color);
+    }
+
+    /**
+     * overrides the toString function
+     * @return the object as a string
+     */
+    @Override
+    public String toString() {
+        return "" + this._ka + " " + this._color;
+    }
+}
